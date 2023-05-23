3dfb08744f20a33bca1a0f91870bf1c609396fbc
almighwar <101317364+almighwar@users.noreply.github.com>
2023-05-23 01:01:26 +0100

    i

--- C05/exc/usrname.c
+++ C05/exc/usrname.c
@@ -4,15 +4,15 @@ int main(void) {
 	char usrname[30];
 	int x = 0;
 
-	
+
 	printf("Username: ");
 	scanf("%s", &usrname);
 
 	for(;usrname[x] != '\0'; x++){
-		
-	} 
-	
+		printf("%i: %c\n", x+1, usrname[x]);
+	}
+
 	printf("%s = %d\n", usrname, x);
-	
+
 	return 0;
 }
