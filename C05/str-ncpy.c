190cb171b83890a8f1dd09005556d68f7de237c1
almighwar <101317364+almighwar@users.noreply.github.com>
2023-05-23 14:21:59 +0100

    Add files via upload

--- C05/str-ncpy.c
+++ C05/str-ncpy.c
@@ -3,12 +3,13 @@
 
 int main(void) {
     char String[] = "Nothing!";
-	char Empty_One[20], Empty_Two[20];
+	char Empty_One[20], Empty_Two[20], Empty_three[20];
 
     strcpy(Empty_One, String);
     strncpy(Empty_Two, String, 5);
+    strcpy(Empty_three, "Nothing here~");
 
-    printf("Empty One = %s\nEmpty Two = %s\n", Empty_One, Empty_Two);
+    printf("Empty One = %s\nEmpty Two = %s\nEmpty Three = %s\n", Empty_One, Empty_Two, Empty_three);
 
 	return 0;
 }
