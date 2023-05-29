a7f07647bd713c676773982c38ce5bf2bce96218
almighwar <101317364+almighwar@users.noreply.github.com>
2023-05-30 00:47:20 +0100

    not comleted

---
+++ C05/exc/cnthello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define rep 20
+
+int main(void) {
+	char arrhelo[rep] = "Hello, World!";
+	int x;
+
+	for(x = 1;arrhelo[x] != '\0'; ++x){
+
+	}
+
+	printf("%s = %d\n",arrhelo, x);
+
+	return 0;
+}
