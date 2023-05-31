5ee0535c5c716e687ededc9a98afa9c4788917fc
almighwar <101317364+almighwar@users.noreply.github.com>
2023-05-31 18:52:44 +0100

    define

---
+++ C02/define.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define WORKER_NUM 12
+#define WORK_PLACE 32 //try to add a var with the same name
+
+int main() {
+    printf("Worker number: %d is working  in the workplace number %d\n", WORKER_NUM,WORK_PLACE);
+
+    return 0;
+}
