40e8c86dd9e20b0fcf7c8330f17b4636edadeda9
almighwar <101317364+almighwar@users.noreply.github.com>
2023-05-31 16:19:54 +0100

    numbers and input

---
+++ C01/input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	char x;
+	int y;
+	
+ 	puts("Enter a littre and number: ");
+ 	scanf(" %c %d", &x, &y);
+ 	
+ 	printf("The Input : %d, %c", y, x);
+ 	
+	return 0;
+}
