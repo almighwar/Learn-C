c5c2ab464c8663e16e5472f9efd1c46bde4113d0
almighwar <101317364+almighwar@users.noreply.github.com>
2023-05-31 13:02:01 +0100

    P and B

---
+++ C01/printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h> //Header file
+
+int main() { //main function and () means that it is without the condition or arguments
+	
+	/* THe First way */
+	printf("Hello World\n"); //function exist in header file <stdio.h>
+	
+	/* The second way */
+	printf("Hello, ");
+	printf("World");
+	
+	/* Another one */
+	printf("Hello. "), printf("World");
+	
+	/* Another one */
+	printf("Hello. \
+			World");
+			
+	return 0;
+}
