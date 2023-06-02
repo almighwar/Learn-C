a9d55bf8f72d410676fac7a9064cdc159ca489d8
almighwar <101317364+almighwar@users.noreply.github.com>
2023-06-02 17:49:24 +0100

    Choas

--- C02/exc/threevar.c
+++ C02/exc/threevar.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-	int x,y,sum;
+	int x,y,equal[5];
 	x = 20;
 	y = 153;
 	
-	sum = x+y;
+	equal[4] = {x+y, x-y, x*y}; //That's not the answer
+	float div = x/y;
 	
-	printf("%d + %d = %d\n", x , y, sum);
-	
+	for(int i=1; x<4; x++){
+		printf("%d + %d = %d\n", x , y, equal[i]);
+		
+		if(i == 3)
+			printf("%d + %d = %f\n", x , y, div);
+		
+	}
 	return 0;
 }
