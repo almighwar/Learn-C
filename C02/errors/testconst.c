954e8480220f19376fbeeb57c8b42c1780a56545
almighwar <101317364+almighwar@users.noreply.github.com>
2023-06-02 16:26:51 +0100

    add some magic

--- C02/errors/testconst.c
+++ C02/errors/testconst.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
+int y; //False, it's not possible to decla the var out of the main function
+      // But const u can with #define
+
 int main(void) {
 	const int x = 7;
 
 	printf("%d\n", x);
 
-	x = 7;
+	x = 7; //False, because the value of x is constant
 	
 	printf("%d\n", x);
 	return 0;
