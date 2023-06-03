ddd07fa2e90fcb1984aaff6c54dd43412a1f8053
almighwar <101317364+almighwar@users.noreply.github.com>
2023-06-03 16:31:20 +0100

    increment and decrement

---
+++ C03/decrement.c
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+
+int main(void) {
+	int x, y;
+	x = 7;
+	
+	printf("x = %d\n", x);
+	
+	x--;
+	
+	printf("x = %d\n", x);
+	
+	--x;
+	
+	printf("x = %d\n", x);
+	
+	x -= 1;
+	
+	printf("x = %d\n", x);
+	
+	printf("x = %d\n", --x);
+	
+	printf("x = %d\n", x--); // the difference between these two increment
+
+	printf("x = %d\n", x -= 1);
+	
+	return 0;
+}
