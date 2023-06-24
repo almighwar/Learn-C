b1b73816d1bf02b6fcbd823d2f43706ce0d6d929
almighwar <100postseveryday@gmail.com>
2023-06-24 19:46:51 +0100

    upda

--- C04/exc/game.c
+++ C04/exc/game.c
@@ -1,60 +1,79 @@
 #include <stdio.h>
 
+// Define clear the console, it's work only in linux
+void cl() {
+	char system(char *command);
+    system("clear");
+}
+
 int main(void) {
     char x;
-    int y, i;
-    char system(char *command);
+    int y;
 
-	void cl() {
-		system("clear");
-	}
-	
-	cl();
-	
-    printf("1: Start Game\n2:Option\n3:About Game\n4:Exit Game\nEnter your choice: ");
-	scanf(" %c", &x);
-	
-	switch(x) {
-		case '1':
-			/* for (i=0; i<65; i++)
-				printf("\f\n"); */	
-			cl();
+    // Clear function 
+    cl();
+
+    // Display the menu options
+    printf("Chess Game\n1: Start Game\n2: Option\n3: About Game\n4: Exit Game\nEnter your choice: ");
+    scanf(" %c", &x);
+
+    // The choice
+    switch (x) {
+        case '1':
+            cl();
+            
+            // Display the game
+            puts("\033[1;33m!Important\033[0m : uppercase: Black peices & lowercase: White pieces\n");
+            puts("[░R][ K][░B][ Q][░K][ B][░K][ R]");
+            puts("[ P][░P][ P][░P][ P][░P][ P][░P]");
+            puts("[░░][  ][░░][  ][░░][  ][░░][  ]");
+            puts("[  ][░░][  ][░░][  ][░░][  ][░░]");
+            puts("[░░][  ][░░][  ][░░][  ][░░][  ]");
+            puts("[  ][░░][  ][░░][  ][░░][  ][░░]");
+            puts("[░p][ p][░p][ p][░p][ p][░p][ p]");
+            puts("[ r][░k][ b][░q][ k][░b][ k][░r]");
+            break;
+
+        case '2':
+            cl();
+            
+            // Display the menu of option menu
+            printf("1: Controls\n2: Display\n3: Sound\nEnter a choice: ");
+
+            // The choice
+            // Another Solution :
+            
+            /*ch = getchar();
+			fflush(stdin);
+			ch = getchar(); */
 			
-			puts("[░R][ K][░B][ Q][░K][ B][░K][ R]");
-			puts("[ P][░P][ P][░P][ P][░P][ P][░P]");
-			puts("[░░][  ][░░][  ][░░][  ][░░][  ]");
-			puts("[  ][░░][  ][░░][  ][░░][  ][░░]");
-			puts("[░░][  ][░░][  ][░░][  ][░░][  ]");
-			puts("[  ][░░][  ][░░][  ][░░][  ][░░]");
-			puts("[░p][ p][░p][ p][░p][ p][░p][ p]");
-			puts("[ r][░k][ b][░q][ k][░b][ k][░r]");
-			break;
+			// To this
+            while ((y = getchar()) != '\n' && y != EOF);
+			y = getchar();
 			
-		case '2':
-			cl();
-			
-			printf("1:Controls\n2:Display\n3:Sound\n");
- 			y = getchar();
- 			
-		    switch(y!=1){
-				case 1:
-					puts("There's no Option, This for exrcice");
-					break;
-				case 0:
-					puts("Also There's no Option, Just exercice");
-					break;
-			}
-			break;
-					
-		case '3':
-			cl();
-			
-			printf("Exercie: Menu Game\nAuthor: almighwar\nAccounts:\n\tLichess: almighwar_1\n\tChess.com: almighwar\nAbout Game: 2D Chess Game to learn chess tikitaka and games and solve puzlle, it'll take another dimensions\n");
-			break;
-		
-		case '4':
-			puts("Exiting...");
-			break;
-	}
-	return (0);
+			// There's no choice
+            switch (y) {
+                case 1:
+                    puts("There's no Option, This for exercise");
+                    break;
+                default:
+                    puts("Also There's no Option, Just exercise");
+                    break;
+            }
+            break;
+
+        case '3':
+            cl();
+
+            // Information about the game
+            printf("Exercise: Menu Game\nAuthor: almighwar\nAccounts:\n\tLichess: almighwar_1\n\tChess.com: almighwar\n\nAbout the game: 2D Chess Game to learn chess tikitaka and games and solve puzzles\n\tit'll take another dimensions\n");
+            break;
+            
+		// Exit the game
+        case '4':
+            puts("Exiting...");
+            break;
+    }
+
+    return 0;
 }
